e0793183190d20d68d924bc81e5dd7af533f30e9
iKeshav <40928377+myself-NEO@users.noreply.github.com>
2022-12-21 02:05:21 +0530

    Structure Updated

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  homePageUrl = "/home";
+  aboutPageUrl = "/about";
+  angularPageUrl = "/angular";
+  springPageUrl = "/spring";
+  contactPageUrl = "/contact";
+
+  homeSelected: Boolean = true; 
+  aboutSelected: Boolean = false;
+  angularSelected: Boolean = false;
+  springSelected: Boolean = false;
+  contactSelected: Boolean = false;
+
+  constructor(
+    private readonly router: Router
+  ) { }
+
+  goToHome() {
+    this.homeSelected = true; 
+    this.aboutSelected = false;
+    this.angularSelected = false;
+    this.springSelected = false;
+    this.contactSelected = false;
+    this.router.navigateByUrl(this.homePageUrl);
+  }
+
+  goToAbout() {
+    this.homeSelected = false; 
+    this.aboutSelected = true;
+    this.angularSelected = false;
+    this.springSelected = false;
+    this.contactSelected = false;
+    this.router.navigateByUrl(this.aboutPageUrl);
+  }
+
+  goToAngular() {
+    this.homeSelected = false; 
+    this.aboutSelected = false;
+    this.angularSelected = true;
+    this.springSelected = false;
+    this.contactSelected = false;
+    this.router.navigateByUrl(this.angularPageUrl);
+  }
+
+  goToSpring() {
+    this.homeSelected = false; 
+    this.aboutSelected = false;
+    this.angularSelected = false;
+    this.springSelected = true;
+    this.contactSelected = false;
+    this.router.navigateByUrl(this.springPageUrl);
+  }
+
+  goToContact() {
+    this.homeSelected = false; 
+    this.aboutSelected = false;
+    this.angularSelected = false;
+    this.springSelected = false;
+    this.contactSelected = true;
+    this.router.navigateByUrl(this.contactPageUrl);
+  }
 
   ngOnInit(): void {
   }
